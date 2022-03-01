2ca84052f4de1d99755ff0703967e7947b75b14d
“Anttiolh” <anttiolh@metropolia.fi>
2022-03-01 12:30:06 +0200

    Main activity update

--- app/src/main/java/com/example/mobts_projekti/MainActivity.java
+++ app/src/main/java/com/example/mobts_projekti/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -26,16 +27,36 @@ public class MainActivity extends AppCompatActivity {
         date.setText(dayString);
 
         final Spinner menu = findViewById(R.id.dropDownMenu);
-        Button button = findViewById(R.id.button);
 
-        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this,R.array.menu, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.menu, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         menu.setAdapter(adapter);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SleepActivity.class);
+        menu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                switch (pos) {
+                    case 0:
+                        break;
+                    case 1:
+                        Intent intent = new Intent(MainActivity.this, SleepActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        intent = new Intent(MainActivity.this, ExerciseActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 3:
+                        intent = new Intent(MainActivity.this, FoodActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 4:
+                        intent = new Intent(MainActivity.this, AchievementsActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+            public void onNothingSelected(AdapterView<?> parent) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
