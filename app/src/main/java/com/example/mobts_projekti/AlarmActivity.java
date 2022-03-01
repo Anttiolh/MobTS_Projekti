a473c095c4e78c01a4cd23c3f7c6c80d5538f206
“Tuomaaaas” <tuomas.mellin@metropolia.fi>
2022-03-01 15:42:07 +0200

    Sleep- ja AlarmActivity

--- app/src/main/java/com/example/mobts_projekti/AlarmActivity.java
+++ app/src/main/java/com/example/mobts_projekti/AlarmActivity.java
@@ -1,13 +1,36 @@
 package com.example.mobts_projekti;
 
-public class AlarmActivity {
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.provider.AlarmClock;
+import android.util.Log;
 
-    public AlarmActivity(){
+import androidx.appcompat.app.AppCompatActivity;
 
+
+public class AlarmActivity extends AppCompatActivity {
+
+    private static final String TAG = "TESTI";
+    private int hour;
+    private int minute;
+
+    public AlarmActivity(int hour, int minute){
+        this.hour = hour;
+        this.minute = minute;
     }
 
+    public AlarmActivity(){
+        this.hour = 0;
+        this.minute = 0;
+    }
     public void setAlarm(){
-
+    Log.d(TAG, "AlarmActivity sets an alarm with these parameters: " + this.hour + this.minute);
+    Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+    intent.putExtra(AlarmClock.EXTRA_HOUR, this.hour);
+    intent.putExtra(AlarmClock.EXTRA_MINUTES,this.minute);
+    startActivity(intent);
     }
 
     public void removeAlarm(){
@@ -19,14 +42,12 @@ public class AlarmActivity {
     }
 
     public void setRingtone(){
-
+        Log.d(TAG, "setRingtone called!");
+        Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+        startActivity(intent);
     }
 
     public void playRingtone(){
-
-    }
-
-    public void setPowerNap(){
 
     }
 }
