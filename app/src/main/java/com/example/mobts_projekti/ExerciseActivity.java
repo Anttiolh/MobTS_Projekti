753b33dcf067cc6b28a18a1fb5f00aad4bed0449
“Anttiolh” <anttiolh@metropolia.fi>
2022-03-02 16:34:41 +0200

    Askelmittari (ei testattu, pitäisi toimia)

--- app/src/main/java/com/example/mobts_projekti/ExerciseActivity.java
+++ app/src/main/java/com/example/mobts_projekti/ExerciseActivity.java
@@ -1,14 +1,78 @@
 package com.example.mobts_projekti;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
 
-public class ExerciseActivity extends AppCompatActivity {
+public class ExerciseActivity extends AppCompatActivity implements SensorEventListener {
+    private TextView tvStepCounter;
+    private SensorManager sensorManager;
+    private Sensor stepCounter;
+    private boolean isCounterPresent;
+    private int stepCount = 0;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)== PackageManager.PERMISSION_DENIED){
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION},0);
+        }
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        tvStepCounter = findViewById(R.id.Steps);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!= null){
+            stepCounter=sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isCounterPresent=true;
+        }else{
+            tvStepCounter.setText("Sensori ei ole aktiivinen");
+            isCounterPresent=false;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor == stepCounter){
+            stepCount = (int) sensorEvent.values[0];
+            tvStepCounter.setText(String.valueOf(stepCount));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+            sensorManager.registerListener(this, stepCounter,SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+            sensorManager.unregisterListener(this,stepCounter);
+        }
     }
 }
