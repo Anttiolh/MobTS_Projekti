d838d472d4f64ef82434a9f7c964e22c90a3f5a5
“Anttiolh” <anttiolh@metropolia.fi>
2022-03-04 17:00:30 +0200

    Sleep activity update

--- app/src/main/java/com/example/mobts_projekti/SleepActivity.java
+++ app/src/main/java/com/example/mobts_projekti/SleepActivity.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.SensorEvent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TimePicker;
 import android.widget.ToggleButton;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 
 import java.util.ArrayList;
 
 public class SleepActivity extends AppCompatActivity {
     private static final String TAG = "TESTI";
-    private ArrayList<Integer> SleepHours = new ArrayList<>();
+    private TimePicker timePicker;
+    private int hour;
+    private int min;
+    private BarChart barChart;
+    SleepHours save;
+
 
 
     @Override
@@ -19,17 +31,38 @@ public class SleepActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
 
+        timePicker = findViewById(R.id.timePicker);
+        barChart = findViewById(R.id.barChart);
+
     }
 
-    private void movementDetector(SensorEvent event){
+    public void setSleepTime(View view){
+        hour = timePicker.getCurrentHour();
+        min = timePicker.getCurrentMinute();
+        save = new SleepHours(hour, min);
+        save.add();
+        Log.d(TAG, "omg");
+        showBarChart();
     }
 
-    public void onToggleClicked(View view){
-        if(((ToggleButton)view).isChecked()){
+    private void showBarChart(){
+        ArrayList<Double>valueList = new ArrayList<>();
+        ArrayList<BarEntry>entries = new ArrayList<>();
+        String title = "Title";
 
-
-        } else {
-
+        for (int i = 0; i<save.getSize();i++){
+            valueList.add(save.getSleepTime(i) * 100.1);
         }
+
+        for (int i = 0; i<valueList.size();i++){
+            BarEntry barEntry = new BarEntry(i, valueList.get(i).floatValue());
+            entries.add(barEntry);
+        }
+
+        BarDataSet barDataSet = new BarDataSet(entries, title);
+
+        BarData data = new BarData(barDataSet);
+        barChart.setData(data);
+        barChart.invalidate();
     }
 }
